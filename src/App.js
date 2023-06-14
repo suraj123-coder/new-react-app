55f901173e515cc49d389ae67777ee66912044fb
Suraj Kawale <srjsuraj1397@gmail.com>
2023-06-15 00:52:20 +0530

    syntax rule..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-function App(){
-  return (<>
-  <h1>hello</h1>
-  </>
+// JSX :: Return HTML
+// SYNTAX RULE :: IF ONE LINER, PARENTHIS ARE OPTIONAL
+// SYNTAX RULE :: FOR MULTI LINER, PARENTHIS ARE REQUIRED!!!
+// SYNTAX :: ()
+// SYNTAX :: <h1>Hello</h1>
+// SYNTAX :: (<h1>Hello</h1>)
+
+function App() {
+  // JSX
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+      mollitia exercitationem veritatis harum illum accusantium earum laborum
+      atque natus a qui repudiandae sapiente quae similique, impedit dolor, eius
+      facere laboriosam.
+    </h1>
   );
 }
+
 export default App;
