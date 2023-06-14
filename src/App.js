b65dc1918170869ebcd5cdfbff9ce8976e286e6c
Suraj Kawale <srjsuraj1397@gmail.com>
2023-06-15 00:56:55 +0530

    jsx as a variable

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
-// JSX :: Return HTML
-// SYNTAX RULE :: IF ONE LINER, PARENTHIS ARE OPTIONAL
-// SYNTAX RULE :: FOR MULTI LINER, PARENTHIS ARE REQUIRED!!!
-// SYNTAX :: ()
-// SYNTAX :: <h1>Hello</h1>
-// SYNTAX :: (<h1>Hello</h1>)
+function App(){
+  let jsx=<h1>hello universe</h1>
 
-function App() {
-  // JSX
-  return (
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-      mollitia exercitationem veritatis harum illum accusantium earum laborum
-      atque natus a qui repudiandae sapiente quae similique, impedit dolor, eius
-      facere laboriosam.
-    </h1>
-  );
+  return jsx;
 }
-
 export default App;
