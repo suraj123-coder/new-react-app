98d754e41afc39aa4609440a33b7fd63e1c3c8ea
Suraj Kawale <srjsuraj1397@gmail.com>
2023-06-15 01:39:02 +0530

    interpolation with object

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 
-
-function App(){
-  let str =`hello world!!!!`;
+// Using Object Literal
+function App() {
+  let str = `Hello Universe!!!`;
   let pi = 3.141159;
-  let active =true;
-  let isUseredLogedIn=true;
+  let user = { id: 1, name: "rohit" };
 
-  return(
+  // JSX :: User Interface
+  return (
     <>
-    <h1>hello universe</h1>
-      <h1>{str}</h1>;
-      <h1>PI={pi}!</h1>
-       
-       {/**IF Logged In */}
-      {isUseredLogedIn && <h1>Welcome to cdac!!</h1>}
-
-        {/**Usage of boolean */}
-
-        <h1>{active ?" Good morning" : "Good night"}</h1>
-        
-         {/** MOST USEFUL  */}
-         {active && <h1>Good morning</h1>}
-         {active ? <h1>Good morning</h1>:<h1>Good night</h1>}
+      <h1>Hello World</h1>
+      <h1>{str}</h1>
+      <h1>PI {pi} !!</h1>
+      <h1>
+        ID: {user.id} Name: {user.name}
+      </h1>
     </>
   );
-
-
 }
 
 export default App;
