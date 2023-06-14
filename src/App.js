0dd1f2cea83eb1f9612d09b8f77d3768495e2470
Suraj Kawale <srjsuraj1397@gmail.com>
2023-06-15 01:12:26 +0530

    formating and suggestion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-function App(){
-  let jsx1 =<h1>Good Morning</h1>;
-  let jsx2 =<h1>Good Night</h1>;
 
-  return false ? jsx1:jsx2;
 
+// is this working in your system ??
+// AUTO SUGGESTION FEATURE ??
+// Auto Formatting. Prettier.
+function App() {
+  return (
+    <>
+      <h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero dolores
+        dolore quam nulla repellendus. Voluptatibus ullam dolorum asperiores
+        beatae dolor, inventore, ducimus corporis esse aspernatur, perferendis
+        ratione id tempore facere?
+      </h1>
+    </>
+  );
 }
+
 export default App;
