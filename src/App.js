4f238ded45e314b13c5d271705bcdfe945e52ae4
Suraj Kawale <srjsuraj1397@gmail.com>
2023-06-15 01:04:45 +0530

    conditional return

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 function App(){
-  let jsx=<h1>hello universe</h1>
+  let jsx1 =<h1>Good Morning</h1>;
+  let jsx2 =<h1>Good Night</h1>;
 
-  return jsx;
+  return false ? jsx1:jsx2;
+
 }
 export default App;
